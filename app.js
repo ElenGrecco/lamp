4393e300ed38b319970598eae69bebb30419df86
Elen Grecco <elen_grecco@hotmail.com>
2024-11-07 20:44:28 -0300

    quebrar lâmpada

--- app.js
+++ app.js
@@ -4,11 +4,12 @@
  */
 
 function quebrar() {
-    document.getElementById('lamp').src="img/broken.jpg"
+    
     //reproduzindo um arquivo de áudio no JS
     //Passo 1: copiar o arquivo de áudio para o projeto
     //Passo 2: usar a classe Audio (biblioteca interna do JS)
     let som = new Audio()
     som.src = "sound/glassbreaking.wav"
     som.play()
+    document.getElementById('lamp').src="img/broken.jpg"
 }
