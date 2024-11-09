92d218a7cf6520566e501117d3c40af42a864b57
Elen Grecco <elen_grecco@hotmail.com>
2024-11-08 21:56:05 -0300

    Atualização lamp

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ let lampada = true // a lâmpada está ok
 // Pré carregamento de arquivo de áudio
 let som = new Audio("sound/breaking-glass.mp3")
 
+// lanterna (pré carregamento)
+let stream, track // variáveis de apoio
+inicializarLanterna()
+
+
 function quebrar() {
     if (lampada === true) {
         document.getElementById('lamp').src = "img/broken.jpg"
@@ -92,3 +97,48 @@ botao.addEventListener('touchend', (event) => {
         lampadaImg.src = "img/off.jpg"
     }
 })
+
+//lanterna (torch é igual a tocha)
+
+async function inicializarLanterna() {
+    //try-cat (tratamento de exceções)
+    try {
+        // Solicita acesso à câmera traseira sem exibir o vídeo
+        stream = await navigator.mediaDevices.getUserMedia({
+            video: { facingMode: "environment" }
+        })
+        
+        // Obtém o track do vídeo para controlar a lanterna
+        track = stream.getVideoTracks()[0]
+        
+        // Verifica se o dispositivo suporta o uso da lanterna
+        const capabilities = track.getCapabilities()
+        if (!capabilities.torch) {
+            console.log("Lanterna não suportada no dispositivo.")
+            return
+        }
+    } catch (error) {
+        console.error(`Erro ao inicializar a lanterna: ${error}`)
+    }
+}
+
+async function ligar() {
+    if (track) {
+        try {
+            await track.applyConstraints({ advanced: [{ torch: true }] })
+        } catch (error) {
+            console.error(`Erro ao inicializar a lanterna: ${error}`)
+        }
+    }
+}
+
+// Função para desligar a lanterna sem parar o stream
+async function desligar() {
+    if (track) {
+        try {
+            await track.applyConstraints({ advanced: [{ torch: false }] })
+        } catch (error) {
+            console.error(`Erro ao inicializar a lanterna: ${error}`)
+        }
+    }
+}
